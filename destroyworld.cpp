ffb62e8287fd4114040210ae346f19d73dba8bb0
ErnestSTA <ernestswbs@gmail.com>
2023-06-23 21:32:39 +0200

    wprowadzono złośliwy kod w programie destroyworld

--- destroyworld.cpp
+++ destroyworld.cpp
@@ -4,5 +4,8 @@ int main() {
     std::cout << "A plan to destroy the world" << std::endl;
     std::cout << "lie down and wait" << std::endl;
     std::cout << "That's it" << std::endl;
+
+    std::cout << "I love pierogi" << std::endl;
+
     return 0;
 }
