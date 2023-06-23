5aa15b3510935cde36d61bff17c015740d1458b6
ErnestSTA <ernestswbs@gmail.com>
2023-06-23 21:45:28 +0200

    Revert "wprowadzono złośliwy kod w programie destroyworld"
    This reverts commit ffb62e8287fd4114040210ae346f19d73dba8bb0.

--- destroyworld.cpp
+++ destroyworld.cpp
@@ -4,8 +4,5 @@ int main() {
     std::cout << "A plan to destroy the world" << std::endl;
     std::cout << "lie down and wait" << std::endl;
     std::cout << "That's it" << std::endl;
-
-    std::cout << "I love pierogi" << std::endl;
-
     return 0;
 }
