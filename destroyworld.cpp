f5c7c4ff8688d2add950000f6d74cf35f01f8133
ErnestSTA <ernestswbs@gmail.com>
2023-06-23 21:28:49 +0200

    dodano zmiany w programie destroyworld

--- destroyworld.cpp
+++ destroyworld.cpp
@@ -2,5 +2,7 @@
 
 int main() {
     std::cout << "A plan to destroy the world" << std::endl;
+    std::cout << "lie down and wait" << std::endl;
+    std::cout << "That's it" << std::endl;
     return 0;
 }
